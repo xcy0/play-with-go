603f0a77f1b71e9391dfbced25fc8f0f0b5ead3c
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-02 19:09:39 +0800

    use type

--- typedef/typedef.go
+++ typedef/typedef.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"play-with-go/typedef/org"
+)
+
 func main() {
+	p := org.NewPerson("James", "Bond")
+	fmt.Println(p.ID())
+	fmt.Println(p)
+
+	p.SetTwitter("@james")
+	fmt.Println(p)
 
 }
