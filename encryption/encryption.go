f68f5438ba93d8814754d75208d00c947ecb6280
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-03 17:05:25 +0800

    encryption

--- encryption/encryption.go
+++ encryption/encryption.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	str := "This is a string"
+	fmt.Println(str)
+
+	encrypted := encrypt(str)
+	fmt.Println(encrypted)
+
+	decrypted := decrypt(encrypted)
+	fmt.Println(decrypted)
+
+}
+
+func encrypt(s string) string {
+	return s
+}
+
+func decrypt(s string) string {
+	return s
 }
