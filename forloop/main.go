b182d509615ce06ec771802a4706f13ce8f11582
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-03 13:37:54 +0800

    show linkedlist

--- forloop/main.go
+++ forloop/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // LinkedList struct
 type LinkedList struct {
@@ -10,9 +14,23 @@ type LinkedList struct {
 
 func main() {
 
-	list := createLinkedList(10, nil)
+	rand.Seed(time.Now().UnixNano())
+
+	list := createLinkedList(rand.Int(), nil)
+
+	for i := 0; i < 5; i++ {
+		list = createLinkedList(rand.Int(), list)
+	}
+	showLinkedList(list)
 	fmt.Println(list)
 
+}
+
+func showLinkedList(list *LinkedList) {
+	for curList := list; curList != nil; {
+		fmt.Printf("%v\n", curList.value)
+		curList = curList.p
+	}
 }
 
 func createLinkedList(number int, list *LinkedList) *LinkedList {
