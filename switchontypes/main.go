f63a270792c97dd2f8fcd45f1a85799fd2765cce
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-03 10:16:48 +0800

    switch

--- switchontypes/main.go
+++ switchontypes/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"play-with-go/switchontypes/org"
+)
+
 func main() {
+
+	idInterface := org.NewSocialSecurityNumber("111-222-333-444")
+	p := org.NewPerson("James", "Bond", idInterface)
+	fmt.Println(p.ID())
+	fmt.Println(p)
+
+	p.SetTwitter("@james")
+	fmt.Println(p)
+	fmt.Println(p.GetTwitter().Redirect())
+
+	fmt.Printf("type of twitterhandler is %T\n", org.TwitterHandler("test"))
 
 }
