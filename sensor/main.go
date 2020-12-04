b7194a7fb1ca2e2ba732d802150455ecfd32c0fc
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-04 08:33:57 +0800

    prime number

--- sensor/main.go
+++ sensor/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+type intSlice []int
+
+func main() {
+	intList := intSlice{24, 46, 65, 86, 97, 123, 241, 376, 651}
+	fmt.Println(intList)
+	for i := 0; i < len(intList); i++ {
+
+	}
 }
