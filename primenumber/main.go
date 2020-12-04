16c587e3d65e2a6a8abc10ebebe1772dbf429691
Xcy0 <24642048+xcy0@users.noreply.github.com>
2020-12-04 08:51:52 +0800

    prime

--- primenumber/main.go
+++ primenumber/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type intSlice []int
 
 func main() {
 	intList := intSlice{24, 46, 65, 86, 97, 123, 241, 376, 651}
 	fmt.Println(intList)
-	for i := 0; i < len(intList); i++ {
 
+	for i := 0; i < len(intList); i++ {
+		isPrime := true
+		for j := 2; j < int(math.Sqrt(float64(intList[i]))); j++ {
+			if intList[i]%j == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Printf("%v is a prime number\n", intList[i])
+		}
 	}
 }
