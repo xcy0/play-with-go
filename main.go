c984a8441e001c46a90e088b8f672deed34aa0b2
xcy0 <24642048+xcy0@users.noreply.github.com>
2021-01-23 16:47:35 +0800

    chore

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	fmt.Println(add(1, 2))
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println(i)
+		}()
+		time.Sleep(time.Microsecond * 10)
+	}
+	time.Sleep(time.Second)
+
+}
+
+func add(p1 float64, p2 float64) float64 {
+	return p1 + p2
 }
